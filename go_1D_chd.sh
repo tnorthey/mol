71c42d9180a48ba6af94c8db6742601aa191ed15
Thomas <thomas.northey@pm.me>
2024-02-07 08:47:43 +0000

    ring closed or open

--- go_1D_chd.sh
+++ go_1D_chd.sh
@@ -2,9 +2,10 @@
 
 step=$1  # define the target frame e.g. 20, 30, 40, 50
 run_id=""$step"_1d"
+ringclosed=$2  # "closed" or "open"
 starting_xyz_file="xyz/start.xyz"
 target_xyz_file="xyz/target_traj099/target_$step.xyz"  # ring-open
-python3 run_1D_chd.py $run_id $starting_xyz_file $target_xyz_file
+python3 run_1D_chd.py $run_id $starting_xyz_file $target_xyz_file $ringclosed
 
 bestxyz=$(ls -1 tmp_/"$step"_1d_???.????????.xyz | head -n 1) 
 bestdat=$(ls -1 tmp_/"$step"_1d_???.????????.dat | head -n 1) 
--- run_1D_chd.py
+++ run_1D_chd.py
@@ -24,8 +24,16 @@ w = wrap.Wrapper()
 run_id = str(sys.argv[1])  # define a string to label the start of the output filenames
 start_xyz_file = str(sys.argv[2])
 target_xyz_file = str(sys.argv[3])
+ringclosed = str(sys.argv[4])
 reference_xyz_file = "xyz/chd_reference.xyz"
 ###################################
+ACH = 100.0
+if ringclosed == 'closed':
+  ACC = 10.0  
+elif ringclosed == 'open':
+  ACC = 0.1
+else:
+  print("ringclosed must equal 'closed' or 'open'")
 
 w.chd_1D(
     run_id,
@@ -33,10 +41,10 @@ w.chd_1D(
     reference_xyz_file,
     target_xyz_file,
     qvector=np.linspace(0.5, 8.0, 76, endpoint=True),
-    sa_nsteps = 8000,
+    sa_nsteps = 1000,
     sa_step_size = 0.01,
     sa_starting_temp = 1.0,
-    sa_harmonic_factor = (0.1, 100.0),
+    sa_harmonic_factor = (ACC, ACH),
     nrestarts = 5,
     non_h_modes_only=True,  # only include "non-hydrogen" modes
 )
