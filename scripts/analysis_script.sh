39eef63895fcac745fdea2ee275f758539a71dab
Thomas <thomas.northey@pm.me>
2024-02-08 15:12:23 +0000

    cleaned up wrap function, plot scripts

--- scripts/analysis_script.sh
+++ scripts/analysis_script.sh
@@ -2,9 +2,10 @@
 
 path=$1  # e.g. ../results_/
 step=$2  # 20, 30 40 50 etc
+description=$3  # append a description to the .dat files
 
-file1=r05_dihedral_"$step".dat
-file2=rmsd_fsignal_"$step".dat
+file1=r05_dihedral_"$step"_"$description".dat
+file2=rmsd_fsignal_"$step"_"$description".dat
 touch $file1 $file2
 rm $file1 $file2
 for i in $path/"$step"_1d_???.*.xyz
