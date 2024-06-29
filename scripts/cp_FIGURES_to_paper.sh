639691ed1aff0154c67e4d6c84dac6d47fe02d0c
Thomas <thomas.northey@pm.me>
2024-06-29 15:39:42 +0100

    more updated fig 1

--- scripts/cp_FIGURES_to_paper.sh
+++ scripts/cp_FIGURES_to_paper.sh
@@ -5,5 +5,5 @@ target_dir="/home/thomas/adam_thomas_paper/figures/"
 cp PLOT_TARGET_FUNCTION_TRAJ09?.png $target_dir
 cp PLOT_RMSD_LOWEST_QUARTILE*.png $target_dir
 cp PLOT_NOISE_DATA.png $target_dir
-cp PLOT_SCATTER_SINGLE_TARGET_NOISE0p0_STRONG_RMSD_FXRAY.png $target_dir
+cp PLOT_SCATTER_SINGLE_TARGET_*.png $target_dir
 cp single_target_*.png $target_dir
