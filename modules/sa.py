fixed factor of 2 with molecular

modules/sa.py
@@ -102,15 +102,9 @@ class Annealing:
         ##=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=##
         ### define qx, qy, qz for Ewald mode
         if ewald_mode:
-            tlen = 1 * qlen
-            plen = 2 * qlen  # double phi-grid points because it spans double
-            th_min, th_max = 0, np.pi
-            ph_min, ph_max = 0, 2 * np.pi
-            th = np.linspace(th_min, th_max, tlen, endpoint=True)
-            ph = np.linspace(
-                ph_min, ph_max, plen, endpoint=False
-            )  # skips 2pi as f(0) = f(2pi)
-            # define coordinates on meshgrid
+            th, ph, qlen, tlen, plen, qmin, qmax, th_min, th_max, ph_min, ph_max = (
+                x.setup_ewald_coords(qvector)
+            )  # setup Ewald sphere coordinates
             r_grid, th_grid, ph_grid = np.meshgrid(qvector, th, ph, indexing="ij")
             # Convert spherical coordinates to Cartesian coordinates
             qx = r_grid * np.sin(th_grid) * np.cos(ph_grid)
@@ -176,7 +170,7 @@ class Annealing:
                             qdij = qvector * LA.norm(xyz_[ii, :] - xyz_[jj, :])
                             molecular += pre_molecular[k, :] * np.sin(qdij) / qdij
                             k += 1
-                iam_ = atomic_total + 2 * molecular + compton
+                iam_ = atomic_total + molecular + compton
                 ##=#=#=# END IAM CALCULATION #=#=#=##
 
                 ##=#=#=# PCD & CHI2 CALCULATIONS #=#=#=##

modules/wrap.py
@@ -92,14 +92,9 @@ class Wrapper:
         electron_mode = False
         twod_mode = False
         aa, bb, cc = x.read_iam_coeffs()
-        tlen = 1 * qlen
-        plen = 2 * qlen  # more grid points in phi because it spans more
-        th_min, th_max = 0, np.pi
-        ph_min, ph_max = 0, 2 * np.pi
-        th = np.linspace(th_min, th_max, tlen, endpoint=True)
-        ph = np.linspace(
-            ph_min, ph_max, plen, endpoint=False
-        )  # skips 2pi as f(0) = f(2pi)
+        th, ph, qlen, tlen, plen, qmin, qmax, th_min, th_max, ph_min, ph_max = (
+            x.setup_ewald_coords(qvector)
+        )
 
         def xyz2iam(xyz, atomic_numbers, compton_array, ewald_mode):
             """convert xyz file to IAM signal"""
@@ -235,6 +230,8 @@ class Wrapper:
         # if not os.path.exists(target_function_file):
         print("Saving data to %s ..." % target_function_file)
         if ewald_mode:
+            ## TO DO: Maybe the error is I "rotavg" the full iam
+            ## before I only rotavg the molecular, atomic and compton separately and then added
             target_function_r = spherical_rotavg(target_function)
             np.savetxt(
                 target_function_file, np.column_stack((qvector, target_function_r))

modules/x.py
@@ -200,6 +200,30 @@ class Xray:
             iam += compton
         return iam, atomic, molecular, compton, pre_molecular
 
+    def setup_ewald_coords(self, qvector):
+        qmin, qmax, qlen = qvector[0], qvector[-1], len(qvector)
+        tlen = 1 * qlen
+        plen = 2 * qlen  # more grid points in phi because it spans more
+        th_min, th_max = 0, np.pi
+        ph_min, ph_max = 0, 2 * np.pi
+        th = np.linspace(th_min, th_max, tlen, endpoint=True)
+        ph = np.linspace(
+            ph_min, ph_max, plen, endpoint=False
+        )  # skips 2pi as f(0) = f(2pi)
+        return (
+            th,
+            ph,
+            qlen,
+            tlen,
+            plen,
+            qmin,
+            qmax,
+            th_min,
+            th_max,
+            ph_min,
+            ph_max,
+        )
+
     def iam_calc_ewald(
         self,
         atomic_numbers,
@@ -212,15 +236,9 @@ class Xray:
         calculate IAM function in the Ewald sphere
         """
         natoms = len(atomic_numbers)
-        qmin, qmax, qlen = qvector[0], qvector[-1], len(qvector)
-        tlen = 1 * qlen
-        plen = 2 * qlen  # more grid points in phi because it spans more
-        th_min, th_max = 0, np.pi
-        ph_min, ph_max = 0, 2 * np.pi
-        th = np.linspace(th_min, th_max, tlen, endpoint=True)
-        ph = np.linspace(
-            ph_min, ph_max, plen, endpoint=False
-        )  # skips 2pi as f(0) = f(2pi)
+        th, ph, qlen, tlen, plen, qmin, qmax, th_min, th_max, ph_min, ph_max = (
+            self.setup_ewald_coords(qvector)
+        )
         # define coordinates on meshgrid
         r_grid, th_grid, ph_grid = np.meshgrid(qvector, th, ph, indexing="ij")
         # Convert spherical coordinates to Cartesian coordinates

tests/test_wrap.py
@@ -30,7 +30,7 @@ def test_wrap_chd():
         reference_xyz_file,
         target_xyz_file,
         results_dir,
-        qvector=np.linspace(1e-9, 2.0, 21, endpoint=True),
+        qvector=np.linspace(1e-9, 4.0, 21, endpoint=True),
         noise = 0.0,
         noise_data_file = "noise/noise.dat",
         inelastic=True,
@@ -41,8 +41,8 @@ def test_wrap_chd():
         hydrogen_modes = np.arange(28, nmodes),  # CHD hydrogen modes
         sa_mode_indices = np.arange(0, nmodes),  # CHD, all modes
         ga_mode_indices = np.arange(0, nmodes),  # CHD, all modes
-        sa_nsteps=40,
-        ga_nsteps=40,
+        sa_nsteps=10000,
+        ga_nsteps=20000,
         ho_indices1 = np.array([[0, 1, 2, 3, 4], [1, 2, 3, 4, 5]]),  # chd (C-C bonds)
         ho_indices2 = np.array([
             [6, 12, 5, 5, 0, 0, 1, 2, 3, 4],